47b02832bd7823aed5a795856a1356de79408db8
manumonforte <manumonforesco2@gmail.com>
2019-01-15 11:10:33 +0100

    enunciados

---
+++ Algoritmos_Voraces/MaratonDeCineDeTerror/MaratonDeCineDeTerror.cpp
@@ -0,0 +1,115 @@
+﻿// Manuel Monforte
+// TAIS62
+
+/*El problmea nos pregunta que a cuantas peliculas puede acceder como maximo Deborah, para ello
+ordenamos las peliculas dependiendo de su hora final de manera creciente. De manera que para cada pelicula
+comprobamos si ocupa parte de la siguiente, si no es asi aumentamos las peliculas que puede ver
+
+La solucion tiene coste o(NlogN) donde N es el numero de peliculas, ya que se realizan operaciones
+push/top sobre la cola de prioridad que tiene logN, y se realizan N veces.*/
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <queue>
+#include <functional>
+struct tHora {
+	int hora;
+	int min;
+
+	tHora() {};
+	tHora(int a, int b) : hora(a), min(b) {};
+};
+
+tHora operator+(tHora const &a, int const &minutos) {
+	tHora horaFin = a;
+	horaFin.min += minutos;
+	while (horaFin.min >= 60) {
+		horaFin.min -= 60;
+		++horaFin.hora;
+	}
+	return horaFin;
+}
+
+bool operator==(tHora const &a, tHora const &b) {
+	return a.hora == b.hora && a.min == b.min;
+}
+
+bool operator<(tHora const &a, tHora const &b) {
+	return (a.hora < b.hora) || (a.hora == b.hora && a.min < b.min);
+}
+
+bool operator>(tHora const &a, tHora const &b) {
+	return (a.hora > b.hora) || (a.hora == b.hora && a.min > b.min);
+}
+
+struct tPelicula {
+	tHora horaIni;
+	tHora horaFin;
+
+	tPelicula() {};
+	tPelicula(tHora ini, tHora fin) : horaIni(ini), horaFin(fin) {};
+};
+
+bool operator < (const tPelicula & a, const tPelicula & b) {
+	return a.horaFin > b.horaFin || (a.horaFin == b.horaFin && a.horaIni > b.horaIni);
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int N, hora, min, duracion;
+	std::cin >> N;
+	if (!N)
+		return false;
+	std::priority_queue<tPelicula, std::vector<tPelicula>, std::less<tPelicula>> cine;
+
+	char aux;
+
+	for (int i = 0; i < N; i++) {
+		std::cin >> hora >> aux >> min >> duracion;
+		tHora ini = { hora, min };
+		tHora fin = ini + duracion;
+		cine.push({ ini ,fin });
+	}
+
+	int numPelis = 1;
+	tPelicula elem = cine.top();
+	cine.pop();
+	while (!cine.empty()) {
+		if (elem.horaFin + 9 < cine.top().horaIni && cine.top().horaIni > elem.horaIni) {
+			elem = cine.top();
+			numPelis++;
+		}
+		cine.pop();
+	}
+
+	std::cout << numPelis << "\n";
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("dats.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ Algoritmos_Voraces/NosInvaden/NosInavden.cpp
@@ -0,0 +1,87 @@
+﻿// Manuel Monforte
+// TAIS62
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+/*
+El problema nos pregunta cuantas ciudades como maximo podemos proteger. Para ello agrupmos por un lado
+los enemigos y por otro nuestros soldados, ordenandolos ambos de manera creciente. De tal forma
+que vamos observando, si nuestro soldado puede con los enemigos, los asignamos y por tanto podemos proteger
+una ciudad más, en otro caso ese "ejército" se ignora y pasamos al siguiente "ejército" ya que tendrá mayor
+numero de efectivos y puede que sea capaz de hacerse cargo del grupo de enemigos.
+
+Coste de la solución--> O(2NlogN + N)-> Ya que aplicamos 2 veces algortimo sort y luego se recorre el vector*/
+
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int N, elem;
+	std::cin >> N;
+	if (!std::cin)
+		return false;
+	
+	std::vector<int> enemigos;
+	std::vector<int> soldados;
+
+	//leemos enemigos
+	for (int i = 0; i < N; i++){
+		std::cin >> elem;
+		enemigos.push_back(elem);
+	}
+	//leemos soldados
+	for (int i = 0; i < N; i++){
+		std::cin >> elem;
+		soldados.push_back(elem);
+	}
+	//ordenamos ambos vectores
+	std::sort(enemigos.begin(), enemigos.end());
+	std::sort(soldados.begin(), soldados.end());
+
+	int i = 0;
+	int j = 0;
+	int ciudades = 0;
+	while (i < N && j < N){
+		if (enemigos[i] <= soldados[j]){
+			i++;
+			j++;
+			ciudades++;
+		}
+		else
+			j++;
+	}
+
+	std::cout << ciudades << "\n";
+
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
